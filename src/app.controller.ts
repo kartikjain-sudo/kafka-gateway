131617dbb3533250c86e382f93baebf2982b5082
kartikjain-sudo <kartikjain42@protonmail.com>
2023-01-19 13:54:15 +0530

    modular

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,26 +1,13 @@
-import { Body, Controller, Get, Inject, OnModuleInit, Post } from '@nestjs/common';
-import { ClientKafka } from '@nestjs/microservices';
+import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
-import { RegisterUserDto } from './dto/register-user.dto';
 
 @Controller()
-export class AppController implements OnModuleInit {
+export class AppController {
   constructor(
     private readonly appService: AppService,
-    @Inject('REGISTRATION_SERVICE') private readonly registerClient: ClientKafka,
-    ) {}
-
-  onModuleInit() {
-    this.registerClient.subscribeToResponseOf('register_user');
-  }
-
+  ) {}
   @Get()
   getHello(): string {
     return this.appService.getHello();
-  }
-
-  @Post('register')
-  async register(@Body() registerUser: RegisterUserDto): Promise<boolean> {
-    return this.appService.register(registerUser);
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -2,24 +2,11 @@ import { Module } from '@nestjs/common';
 import { ClientsModule, Transport } from '@nestjs/microservices';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { GatewayModule } from './gateway/gateway.module';
 
 @Module({
   imports: [
-    ClientsModule.register([
-      {
-        name: 'REGISTRATION_SERVICE',
-        transport: Transport.KAFKA,
-        options: {
-          client: {
-            clientId: 'registration',
-            brokers: ['localhost:9092'],
-          },
-          consumer: {
-            groupId: 'REGISTRATION_SERVICE_GROUP',
-          },
-        },
-      },
-    ]),
+    GatewayModule,
   ],
   controllers: [AppController],
   providers: [AppService],
