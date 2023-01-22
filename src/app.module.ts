1dc32b1d76aa00494b62f58140ccd910533fae67
kartikjain-sudo <kartikjain42@protonmail.com>
2023-01-22 22:38:29 +0530

    added authentication template

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,12 @@ import { ClientsModule, Transport } from '@nestjs/microservices';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GatewayModule } from './gateway/gateway.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     GatewayModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
